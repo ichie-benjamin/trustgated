fd5ac6d3ef1df2691c886100ed42dc263d227387
Benjamin Chukwudi <benjaminchukwudi0@gmail.com>
2020-10-12 00:01:11 +0100

    fixed homepage style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,8 +30,8 @@
 	<link rel="stylesheet" type="text/css" href="/assets/css/bootstrap4.min.css">
 	<link rel="stylesheet" type="text/css" href="/assets/css/bootstrap-select.min.css">
 	<link rel="stylesheet" type="text/css" href="/assets/css/style.css">
-	<link rel="stylesheet" type="text/css" href="http://74.124.215.220/~demolin/demo/entrepreneur_job_portal/asset/css/templete.css">
-	<link class="skin" rel="stylesheet" type="text/css" href="http://74.124.215.220/~demolin/demo/entrepreneur_job_portal/css/skin/skin-1.css">
+	<link rel="stylesheet" type="text/css" href="/assets/css/template.css">
+{{--	<link class="skin" rel="stylesheet" type="text/css" href="http://74.124.215.220/~demolin/demo/entrepreneur_job_portal/css/skin/skin-1.css">--}}
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
