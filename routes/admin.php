9b7fd2173968305979c5767bfbc4be7b89b5f685
Benjamin Chukwudi <benjaminchukwudi0@gmail.com>
2020-10-16 18:06:47 +0100

    color change

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\admin\AdController;
 use App\Http\Controllers\admin\EducationalDetails;
 use App\Http\Controllers\admin\JobsController;
 use App\Http\Controllers\admin\ProductsController;
@@ -20,6 +21,7 @@ Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], 
 //    User Listing
 
     Route::get('user/admins', [UsersController::class, 'admins'])->name('user.admins');
+    Route::get('user/sub_admins', [UsersController::class, 'subAdmins'])->name('user.sub_admins');
     Route::get('user/employers', [UsersController::class, 'employers'])->name('user.employers');
     Route::get('user/jobseekers', [UsersController::class, 'jobseekers'])->name('user.jobseekers');
     Route::get('user/jobseeker', [UsersController::class, 'jobseekers'])->name('user.jobseeker');
@@ -36,6 +38,7 @@ Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], 
         'functional-area'   => FunctionalAreaController::class,
         'educational-details' => EducationalDetails::class,
         'products' => ProductsController::class,
+        'ads' => AdController::class,
     ]);
 
 });
