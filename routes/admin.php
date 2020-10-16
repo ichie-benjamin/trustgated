0ec84bc1de7909a3c0587c28f2791f38d1eb306a
Emmanuel <emmagab38@gmail.com>
2020-10-16 19:29:11 +0100

    admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\admin\AdController;
 use App\Http\Controllers\admin\EducationalDetails;
 use App\Http\Controllers\admin\JobsController;
-use App\Http\Controllers\admin\PackagesController;
 use App\Http\Controllers\admin\ProductsController;
 use App\Http\Controllers\admin\UsersController;
 use App\Http\Controllers\AdminDashboard;
@@ -22,11 +21,7 @@ Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], 
 //    User Listing
 
     Route::get('user/admins', [UsersController::class, 'admins'])->name('user.admins');
-<<<<<<< HEAD
-    Route::get('user/sub-admins', [UsersController::class, 'admins'])->name('user.sub_admins');
-=======
     Route::get('user/sub_admins', [UsersController::class, 'subAdmins'])->name('user.sub_admins');
->>>>>>> 9b7fd2173968305979c5767bfbc4be7b89b5f685
     Route::get('user/employers', [UsersController::class, 'employers'])->name('user.employers');
     Route::get('user/jobseekers', [UsersController::class, 'jobseekers'])->name('user.jobseekers');
     Route::get('user/jobseeker', [UsersController::class, 'jobseekers'])->name('user.jobseeker');
@@ -43,11 +38,7 @@ Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], 
         'functional-area'   => FunctionalAreaController::class,
         'educational-details' => EducationalDetails::class,
         'products' => ProductsController::class,
-<<<<<<< HEAD
-        'packages' => PackagesController::class
-=======
         'ads' => AdController::class,
->>>>>>> 9b7fd2173968305979c5767bfbc4be7b89b5f685
     ]);
 
 });
