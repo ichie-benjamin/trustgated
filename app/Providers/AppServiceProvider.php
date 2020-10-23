73e0096805dec1565ba88d7e1e31fe89935ba62c
Benjamin Chukwudi <benjaminchukwudi0@gmail.com>
2020-10-23 23:38:59 +0100

    all

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,16 +28,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-//        $f_companies = Cache::remember('f_companies', 3600, function () {
-//            return Company::select('name','id','logo')->inRandomOrder()->limit(6)->get();
-//        });
-//        $ad_job_right = Cache::remember('ad_job_right', 3600, function () {
-//            return Ad::select('company_name','image','position')->whereStatus(true)->wherePosition('job_right')->inRandomOrder()->limit(3)->get();
-//        });
-//        $ad_job_left = Cache::remember('ad_job_left', 3600, function () {
-//            return Ad::select('company_name','image','position')->whereStatus(true)->wherePosition('job_left')->inRandomOrder()->limit(3)->get();
-//        });
+        $f_companies = Cache::remember('f_companies', 3600, function () {
+            return Company::select('name','id','logo')->inRandomOrder()->limit(6)->get();
+        });
+        $ad_job_right = Cache::remember('ad_job_right', 3600, function () {
+            return Ad::select('company_name','image','position')->whereStatus(true)->wherePosition('job_right')->inRandomOrder()->limit(3)->get();
+        });
+        $ad_job_left = Cache::remember('ad_job_left', 3600, function () {
+            return Ad::select('company_name','image','position')->whereStatus(true)->wherePosition('job_left')->inRandomOrder()->limit(3)->get();
+        });
         Schema::defaultStringLength(191);
-//        View::share(['f_companies' => $f_companies, 'ad_job_right' => $ad_job_right, 'ad_job_left' => $ad_job_left]);
+        View::share(['f_companies' => $f_companies, 'ad_job_right' => $ad_job_right, 'ad_job_left' => $ad_job_left]);
     }
 }
