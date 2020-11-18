b5e3fb61da8021eb2e841d379f3d60736e6298f1
ben <ben@gmail.com>
2020-11-18 07:14:51 +0100

    cache

--- app/Models/Highligh.php
+++ app/Models/Highligh.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Traits\CacheClear;
+
 class Highligh extends Model
 {
     use HasFactory;
+    use CacheClear;
 
     protected $guarded = [];
 }
---
+++ app/Traits/CacheClear.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Artisan;
+
+trait CacheClear
+{
+
+    /**
+     * Boot function for Laravel model events.
+     * https://laravel.com/docs/5.8/eloquent#events
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        /**
+         * After model is created, or whatever action, clear cache.
+         */
+        static::updated(function () {
+            Artisan::call('cache:clear');
+        });
+    }
+}
